671bd506be05862cd6c7aa6dc76419b5cabee796
Rofiq Ahmad Mubarok <rofiqahmadm@gmail.com>
2023-02-22 22:22:48 +0700

    update destination data information

--- src/data/destinations.ts
+++ src/data/destinations.ts
@@ -9,7 +9,7 @@ Kuta Beach is very crowded with tourists, especially during the holiday season. 
     shortDescription:
       "Kuta Beach is a beach located in the southern part of Bali, Indonesia. It is one of the most famous beaches in Bali and a popular tourist destination throughout Indonesia.",
     image: "",
-    price: 700000,
+    price: 2000000,
     rating: 5,
     city: "Denpasar",
     location: "Denpasar, Bali",
@@ -30,15 +30,21 @@ Kuta Beach is very crowded with tourists, especially during the holiday season. 
   },
   {
     city: "63f03c77258e70eb5584a986",
-    description: "",
+    description: `Mount Bromo, also known as Gunung Bromo, is an active volcano located in East Java, Indonesia. It is part of the Bromo Tengger Semeru National Park and is one of the most popular tourist destinations in Indonesia.
+
+Mount Bromo stands at a height of 2,329 meters (7,641 feet) and is known for its breathtaking views and unique landscape. The volcano is surrounded by a sea of sand, which stretches for about 10 square kilometers (4 square miles), and is surrounded by steep cliffs. Visitors can climb up to the crater of the volcano and take in the stunning views of the surrounding landscape.
+
+The best time to visit Mount Bromo is during the dry season, which runs from May to October. However, visitors should be aware that the weather can be unpredictable, and the volcano may be closed to visitors if there is a high level of volcanic activity.
+
+Overall, Mount Bromo is a must-visit destination for nature lovers and adventure seekers visiting Indonesia.`,
     image:
       "https://ik.imagekit.io/travelscope/aldrin-rachman-pradana-UEwMB7Z1pKQ-unsplash_1_.jpg",
 
-    name: "Gunung Bromo",
+    name: "Mount Bromo",
     price: 1000000,
     rating: 4,
     shortDescription:
-      "Gunung Bromo adalah sebuah gunung berapi yang terletak di Jawa Timur, Indonesia. Gunung Bromo terkenal dengan pemandangan kawahnya yang indah dan pesona matahari terbitnya yang memukau.",
+      "Mount Bromo, also known as Gunung Bromo, is an active volcano located in East Java, Indonesia. It is part of the Bromo Tengger Semeru National Park and is one of the most popular tourist destinations in Indonesia.",
   },
   {
     city: "",
